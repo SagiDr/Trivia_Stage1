e9a3a69ad1196ce114d93124dd1825fcb79d1b3e
RoeiDayan <roeidayan2007@gmail.com>
2023-11-28 14:57:46 +0200

    Wrote the entire SQL database Ready for scaffold

--- Trivia_Stage1/DBScripts/Create.sql
+++ Trivia_Stage1/DBScripts/Create.sql
@@ -8,33 +8,49 @@ Create TABLE userDB (
 [UserId] INT IDENTITY (1, 1) NOT NULL,
 [UserMail] NVARCHAR(30) NOT NULL Unique,
 [UserName] NCHAR (20) NOT NULL,
-[UserRankID] INT,
-[Score] int,
-Primary Key ([USerID])
-CONSTRAINT [PK_userDB] PRIMARY KEY CLUSTERED ([UserId] ASC),
+[UserRankID] INT NOT NULL,
+[Score] INT NOT NULL,
+Primary Key ([USerID]),
+FOREIGN KEY ([UserRankID]) REFERENCES userRankDB([UserRankID])
 
 );
 Go
 
-Create TABLE QuestDB (
+Create TABLE questionsDB (
+[QuestionID] INT Identity(1,1) NOT NULL,
+[QuestionStatusID] INT NOT NULL,
+[USerID] int NOT NULL,
+[SubjectID] int NOT NULL,
+[Text] NVARCHAR(200) NOT NULL,
+[CorrectAns] NVARCHAR(200) NOT NULL,
+[WrongAns1] NVARCHAR(200) NOT NULL,
+[WrongAns2] NVARCHAR(200) NOT NULL,
+[WrongAns3] NVARCHAR(200) NOT NULL,
 
-
+PRIMARY KEY ([QuestionID]),
+FOREIGN KEY ([QuestionStatusID]) REFERENCES questionStatusDB([QuestionStatusID]),
+FOREIGN KEY ([USerID]) REFERENCES userDB([UserId]),
+FOREIGN KEY ([SubjectID]) REFERENCES subjectDB([SubjectID]),
 );
 Go
 
-Create TABLE SubjectDB (
-
-
+Create TABLE subjectDB (
+[SubjectID] int NOT NULL,
+[Subject] NVARCHAR(20) NOT NULL
+PRIMARY KEY ([SubjectID])
 );
 Go
 
-Create TABLE UserRankDB (
-
-
+Create TABLE userRankDB (
+[UserRankID] INT NOT NULL,
+[RankName] NVARCHAR(20) NOT NULL
+PRIMARY KEY ([UserRankID])
 );
 Go
 
-Create TABLE QuestionStatusDB (
-
+Create TABLE questionStatusDB (
+[QuestionStatusID] INT NOT NULL,
+[Status] NVARCHAR(20) NOT NULL,
+PRIMARY KEY ([QuestionStatusID])
 );
 Go
