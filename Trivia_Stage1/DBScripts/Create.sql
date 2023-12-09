335cc7cdf5ed7068084134712ece9946d43c4ce0
SagiDr <sagi16121960@gmail.com>
2023-12-09 22:43:14 +0200

    Fixed SQL

--- Trivia_Stage1/DBScripts/Create.sql
+++ Trivia_Stage1/DBScripts/Create.sql
@@ -37,8 +37,6 @@ FOREIGN KEY ([UserRankID]) REFERENCES userRankDB([UserRankID])
 );
 Go
 
-
-
 Create TABLE questionsDB (
 [QuestionID] INT Identity(1,1) NOT NULL,
 [QuestionStatusID] INT NOT NULL,
@@ -57,36 +55,6 @@ FOREIGN KEY ([SubjectID]) REFERENCES subjectDB([SubjectID]),
 );
 Go
 
-
-
-INSERT INTO [userDB] ([UserMail], [UserName], [UserRankID], [Score], [Password]) VALUES 
-            ('amitayzic12@gmail.com', 'amitayTHEKING ', 1, 5, 'Amitay1');
-
-INSERT INTO [userDB] ([UserMail], [UserName], [UserRankID], [Score],[Password]) VALUES 
-            ('sagidrori@gmail.com', ' sagiTHEKING ', 2, 5, 'Sagi1');
-
-INSERT INTO [userDB] ([UserMail], [UserName], [UserRankID], [Score],[Password]) VALUES 
-            ('roeydayan@gmail.com', ' RoeyTHEKING ', 2, 5, 'Dayan1');
-
-INSERT INTO [userDB] ([UserMail], [UserName], [UserRankID], [Score],[Password]) VALUES 
-            ('amithacham@gmail.com', ' amitTHEKING ', 1, 5, 'Amit1');
-INSERT INTO [userDB] ([UserMail], [UserName], [UserRankID], [Score],[Password]) VALUES 
-            ('a@a.com', ' a ', 1, 5, 'a');
-
-
-INSERT INTO [questionsDB] 
-([QuestionStatusID], [UserID], [SubjectID], [Text], [CorrectAns],[WrongAns1],[WrongAns2],[WrongAns3]) VALUES 
-(1, 1,3, 'When was Israel establish? ', '1948', '1946','1776','1952');
-INSERT INTO [questionsDB] ([QuestionStatusID], [UserID], [SubjectID], [Text], [CorrectAns],[WrongAns1],[WrongAns2],[WrongAns3]) VALUES 
-(2, 2, 1, 'Who won the 2022 FIFA World Cup', 'Argentina', 'France','Algeria','Portugal');
-INSERT INTO [questionsDB] ([QuestionStatusID], [UserID], [SubjectID], [Text], [CorrectAns],[WrongAns1],[WrongAns2],[WrongAns3]) VALUES 
-(2, 3,4, 'What kills you first in the vaccum of space', 'Lack of oxygen', 'The cold','Your blood boiling','Radiation');
-INSERT INTO [questionsDB] ([QuestionStatusID], [UserID], [SubjectID], [Text], [CorrectAns],[WrongAns1],[WrongAns2],[WrongAns3]) VALUES 
-(3, 4,5, 'Who is the principal', 'Hannah Daldi-Mandingo', 'Raheli Lizerovitz','Alex Leorian','Smadar Vechter');
-
-
-
-
 INSERT INTO [subjectDB]
 ([SubjectID], [Subject]) VALUES
 (1, 'Sport')
@@ -103,7 +71,6 @@ INSERT INTO [subjectDB]
 ([SubjectID], [Subject]) VALUES
 (5, 'Ramon Highschool')
 Go
-
 
 INSERT INTO [userRankDB]
 ([UserRankID], [RankName]) VALUES
@@ -126,6 +93,7 @@ INSERT INTO [questionStatusDB]
 ([QuestionStatusID], [Status]) VALUES
 (3, 'Not Accpted')
 Go
+
 INSERT INTO [userDB] ([UserMail], [UserName], [UserRankID], [Score], [Password]) VALUES 
             ('amitayzic12@gmail.com', 'amitayTHEKING ', 1, 5, 'Amitay1');
 
@@ -139,6 +107,7 @@ INSERT INTO [userDB] ([UserMail], [UserName], [UserRankID], [Score],[Password]) 
             ('amithacham@gmail.com', ' amitTHEKING ', 1, 5, 'Amit1');
 Go
 
+
 INSERT INTO [questionsDB] 
 ([QuestionStatusID], [UserID], [SubjectID], [Text], [CorrectAns],[WrongAns1],[WrongAns2],[WrongAns3]) VALUES 
 (1, 1,3, 'When was Israel establish? ', '1948', '1946','1776','1952');
@@ -150,14 +119,8 @@ INSERT INTO [questionsDB] ([QuestionStatusID], [UserID], [SubjectID], [Text], [C
 (3, 4,5, 'Who is the principal', 'Hannah Daldi-Mandingo', 'Raheli Lizerovitz','Alex Leorian','Smadar Vechter');
 Go
 
-
-
-
-
 select * from userDB
 
 select * from questionsDB
 
 select * from questionStatusDB
-
-
